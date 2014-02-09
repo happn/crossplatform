e37a3082e77075b2384b6f8207a971a62f4ebbd4
Benjamin Ruoff <benjamin.ruoff@meetnow.eu>
2014-02-09 22:42:20 +0100

    working camera plugin

--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -1,10 +1,13 @@
 /Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes.dex : \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/BuildConfig.class \
 /Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/Happn.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/R$attr.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/R$drawable.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/R$string.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/R$xml.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/de/zencoding/happn/R.class \
 /Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/Happn.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/R$attr.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/R$drawable.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/R$string.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/R$xml.class \
-/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/io/cordova/hellocordova/R.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/org/apache/cordova/camera/CameraLauncher.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/org/apache/cordova/camera/ExifHelper.class \
+/Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/classes/org/apache/cordova/camera/FileHelper.class \
 /Users/zentur/Documents/Projects/happn-crossplatform/platforms/android/bin/dexedLibs/classes-db64f4d439fca863f4a92888cf6867a3.jar \
